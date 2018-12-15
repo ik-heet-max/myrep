4653368d3caf456c0e31182da500f7c80f53ca59
Max <625746@mail.ru>
2018-12-15 21:28:41 +0300

    теперь программа не разрешает вводить отрицательные числа

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,14 @@
 def fibonacci(n):
     if n == 1:
         return 1
-    if n == 2:
+    elif n == 2:
         return 2
+    elif n < 1:
+        print("You cannot enter negative numbers")
     else:
         return fibonacci(n - 2) + fibonacci(n - 1)
 
 
-f = int(input("Enter a positive integer" )
-print(fibonacci(f))
+while True:
+    f = int(input("Enter a positive integer "))
+    print(fibonacci(f))
