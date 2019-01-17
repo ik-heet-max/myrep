63d1dce13cb4910fcae9029f03f0913345889e4f
Max <625746@mail.ru>
2019-01-18 00:49:08 +0300

    дз 6

---
+++ triangle.py
@@ -0,0 +1,21 @@
+from math import sqrt
+
+
+class Triangle:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        if self.a + self.b <= self.c or self.a + self.c <= self.b or self.b + self.c <= self.a:
+            print("These segments won't make a triangle")
+            exit(1)
+
+    def area(self):
+        half_perimeter = self.perimeter() / 2
+        return sqrt(half_perimeter*(half_perimeter - self.a)*(half_perimeter - self.b)*(half_perimeter - self.c))
+
+    def perimeter(self):
+        return self.a + self.b + self.c
+
+    def sides(self):
+        print("The segments of the triangle are equal", self.a, ",", self.b, "and", self.c)
